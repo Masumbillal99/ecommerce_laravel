a2c53d13ea93875590cfddba1be252c116e35609
masum <mbillal.eee@gmail.com>
2022-04-19 12:16:47 +0600

    crud read

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,7 +16,38 @@
 
                    <p>Welcome, {{ Auth::user()->name }}</p>
                    <p>Welcome, {{ Auth::user()->email }}</p>
-                   <p>Welcome, {{ Auth::user()->creat_at }}</p>
+                   <p>Welcome, {{ Auth::user()->created_at }}</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row mt-3">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header bg-success">
+                    User list
+                </div>
+                <div class="card-body">
+                <table class="table">
+                    <thead>
+                        <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Createed at</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($users as $user)
+                        <tr>
+                        <th scope="row">{{ $user->id }}</th>
+                        <td> {{ $user->name }} </td>
+                        <td>{{ $user->email }}</td>
+                        <td>{{ $user->created_at->format('d/m/Y h:i:s A') }}</td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                    </table>
                 </div>
             </div>
         </div>
